a16c5c206bdd7adffa2b907653357943520c2669
小A <bxa@biaoxiaoa.top>
2019-11-11 18:00:39 +0800

    【用户名】格式前端校验

--- public/static/js/login.js
+++ public/static/js/login.js
@@ -1,3 +1,90 @@
 function verify_code() {
     $('#captchaImg').attr('src', '/coder');
 }
+layui.use(['form', 'layer'], function () {
+    var form = layui.form;
+    var layer = layui.layer;
+    var length = new RegExp(/^[\S]{5,15}$/);
+    form.verify({
+        username:function(value,item){
+            if(value.length==0){
+                return "请输入用户名";
+            }
+            var begin = /^[a-zA-Z]/;//以字母开头
+            if(!begin.test(value)){
+                return "用户名必须以字母开头";
+            }
+            if (!length.test(value)) {
+                return "用户名必须5到15位之间";
+            }
+            var specialCharacters = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>《》/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？]");//不包含特殊符号返回True
+            if (specialCharacters.test(value)) {
+                return "用户名不能输入特殊字符";
+            }
+            return "aaa";
+        },
+        password:function(value,item){
+            if(value.length==0){
+                return "请输入密码";
+            }
+            if (!length.test(value)) {
+                return "密码必须5到20位";
+            }
+        },
+        vercodes:function(value,item){
+            if(value.length==0){
+                return "请输入验证码";
+            }
+            if(value.length!=5){
+                return "验证码5个字符";
+            }
+        }
+    })
+    form.on('submit(login)', function (data) {
+        $.ajax({
+            type:'post',
+            url:'/index.php/index/index/test',
+            success:function(response){
+                console.log(response);
+            },
+            error:function(errors){
+                console.log(errors);
+            }
+        })
+        /*
+        $('#login').attr('disabled',true);
+        $.ajax({
+            type: 'post',
+            url: '/submit_login',
+            data: data.field,
+            success: function (response) {
+                console.log(response);
+                $('#login').removeAttr('disabled');
+                if($('#vercode').val().length>0){
+                    verify_code();
+                }
+                if (response.code != 2000) {
+                    layer.open({
+                        title: '友情提示',
+                        content: response.msg,
+                        icon: 2
+                    });
+                }else{
+                    window.location.href = '/index.php';
+                }
+            },
+            error: function (error) {
+                $('#login').removeAttr('disabled');
+                if($('#vercode').val().length>0){
+                    verify_code();
+                }
+                layer.open({
+                    title: '友情提示',
+                    content: '请求失败',
+                    icon: 2
+                });
+            }
+        })*/
+        return false;
+    })
+});
